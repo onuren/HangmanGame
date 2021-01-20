9f5df0118fe720c8fa31ecd63d5e8d2e9a884b46
onur engun <onurengun@outlook.com>
2021-01-20 03:46:30 +0300

    Add project files.

---
+++ adamAsmacaOyunu/Program.cs
@@ -0,0 +1,97 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace adamAsmacaOyunu
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            islemler a = new islemler();
+            a.baslangic();
+            a.kelimeDizi = new List<char>();
+            a.gecici = new List<char>();
+            char harfTahmin;
+
+            Console.Clear();
+            Console.WriteLine("Zorluk seviyesi: " + a.zorlukSecimStr);
+            Console.WriteLine("-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-");
+            Console.WriteLine("Harf tahmini yaparak oyuna başlayınız:");
+
+            //gecici diziye kelimenin harfleri yerine _ ve / eklenmesi
+            for (int i = 0; i < a.girilenKelime.Length; i++)
+            {
+                a.kelimeDizi.Add(a.girilenKelime[i]);
+                if (a.kelimeDizi[i] == ' ')
+                    a.gecici.Add('/');
+                else
+                    a.gecici.Add('_');
+            }
+
+            for (int i = a.canHakki; i >= 0;)
+            {
+            here:
+                try
+                {
+                    Console.WriteLine("Kalan canınız: " + i + " / " + a.canHakki);
+
+                    //geciciyi yazdır
+                    foreach (char item in a.gecici)
+                    {
+                        Console.Write(item + " ");
+                    }
+                    Console.WriteLine();
+
+                    //harfi al
+                    harfTahmin = Convert.ToChar(Console.ReadLine());
+                    Console.Clear();
+
+                    //harfi içeriyorsa geciciye ekle
+                    for (int j = 0; j < a.kelimeDizi.Count; j++)
+                    {
+                        int ara = a.kelimeDizi.IndexOf(harfTahmin, j);
+                        if (ara >= 0)
+                        {
+                            a.gecici[ara] = harfTahmin;
+                        }
+                        else
+                        {
+                            break;
+                        }
+                    }
+
+                    //içermiyorsa 1 can düşür
+                    if (!(a.kelimeDizi.Contains(harfTahmin)))
+                    {
+                        i--;
+                        Console.WriteLine("Yanlış tahmin.");
+                    }
+
+                    //eğer _ yoksa oyunu kazandınız yaz
+                    if (!(a.gecici.Contains('_')))
+                    {
+                        Console.WriteLine("Tebrikler kelimeyi buldunuz!");
+                        Console.WriteLine();
+                        break;
+                    }
+                }
+                catch
+                {
+                    Console.WriteLine("Lütfen yalnızca harf girişi yapınız.");
+                    goto here;
+                }
+
+            }
+
+            //eğer _ varsa oyunu kaybettiniz yaz
+            if (a.gecici.Contains('_'))
+            {
+                Console.WriteLine("Malesef tahmin hakkınız doldu. \nOyunu kaybettiniz.");
+            }
+        }
+    }
+}
---
+++ adamAsmacaOyunu/islemler.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace adamAsmacaOyunu
+{
+    class islemler
+    {
+        public int zorlukSecim { get; set; }
+        public string zorlukSecimStr { get; set; }
+        public string girilenKelime { get; set; }
+        public List<char> kelimeDizi { get; set; }
+        public List<char> gecici { get; set; }
+        public int canHakki { get; set; }
+
+        public void baslangic()
+        {
+            here:
+            Console.WriteLine("Lütfen Zorluk Seviyesini Seçiniz:");
+            Console.WriteLine("-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-");
+            Console.WriteLine("\t1. Kolay (20 can)");
+            Console.WriteLine("\t2. Orta  (10 can)");
+            Console.WriteLine("\t3. Zor   (5 can)");
+            Console.WriteLine("\t4. Uzman (2 can)");
+            Console.WriteLine("-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-");
+            try
+            {
+                this.zorlukSecim = Convert.ToInt32(Console.ReadLine());
+                Console.Clear();
+                switch (zorlukSecim)
+                {
+                    case 1:
+                        zorlukSecimStr = "Kolay";
+                        canHakki = 20;
+                        break;
+                    case 2:
+                        zorlukSecimStr = "Orta";
+                        canHakki = 10;
+                        break;
+                    case 3:
+                        zorlukSecimStr = "Zor";
+                        canHakki = 5;
+                        break;
+                    case 4:
+                        zorlukSecimStr = "Uzman";
+                        canHakki = 2;
+                        break;
+                }
+
+                Console.WriteLine("Zorluk seviyesi: "+zorlukSecimStr);
+                Console.WriteLine("-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-");
+                Console.WriteLine("Bir Kelime veya Cümle Giriniz:");
+                girilenKelime = Console.ReadLine();
+
+            }
+            catch
+            {
+                Console.WriteLine("Lütfen 1-4 arasında zorluk derecesi seçiniz.");
+                goto here;
+            }
+        }
+
+
+    }
+}
